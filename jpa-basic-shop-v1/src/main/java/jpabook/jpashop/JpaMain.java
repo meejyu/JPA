4a343c1d55921cb378aca8617c34b4fe885f54df
meejyu <jua04016@naver.com>
2022-03-23 14:06:09 +0900

    @ManyToOne의 이해 및 테스트 코드

--- jpa-basic-shop-v1/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpa-basic-shop-v1/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,11 +1,13 @@
 package jpabook.jpashop;
 
 import jpabook.jpashop.domain.Book;
+import jpabook.jpashop.domain.Category;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 /**
  EntityManagerFactory 애플리케이션이 로딩되는 시점에 딱 1개만 만들어야 한다.
@@ -26,18 +28,45 @@ public class JpaMain {
         tx.begin();
 
         // 네이티브 쿼리는 jpa쿼리를 타지 않는다. 인서트, 딜리트를 안해줘서 따로 플러시 해줘야한다.
-
         try {
+            Category category1 = new Category();
+            category1.setName("인형");
+            em.persist(category1);
 
-            Book book = new Book();
-            book.setPrice(10000);
-            book.setStockQuantity(100);
-            book.setName("어린왕자");
-            book.setAuthor("생텍쥐페리");
-            book.setIsbn("9791186288139");
+            Category category2 = new Category();
+            category2.setName("동물");
+            category2.setParent(category1);
+            em.persist(category2);
 
-            em.persist(book);
+            Category category3 = new Category();
+            category3.setName("육식동물");
+            category3.setParent(category2);
+            em.persist(category3);
 
+            Category category4 = new Category();
+            category4.setName("초식동물");
+            category4.setParent(category2);
+            em.persist(category4);
+
+            em.flush();
+            em.clear();
+
+            Category depth1 = em.find(Category.class, category1.getId());
+            List<Category> child = depth1.getChild();
+            for (Category category : child) {
+                System.out.println("depth1.child.getName() = " + category.getName());
+            }
+            Category depth2 = em.find(Category.class, category2.getId());
+            Category parent = depth2.getParent();
+            List<Category> child1 = depth2.getChild();
+            System.out.println("depth2.parent = " + parent.getName());
+            for (Category aa : child1) {
+                System.out.println("depth2.child.getName() = " + aa.getName());
+            }
+            Category depth3 = em.find(Category.class, category3.getId());
+            System.out.println("depth3.parent = " + depth3.getParent().getName());
+            System.out.println("depth3.getParent().getParent().getName() = " + depth3.getParent().getParent().getName());
+            
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- jpa-basic-shop-v1/src/main/java/jpabook/jpashop/domain/Category.java
+++ jpa-basic-shop-v1/src/main/java/jpabook/jpashop/domain/Category.java
@@ -13,6 +13,12 @@ public class Category extends BaseEntity {
 
     private String name;
 
+    /*
+    카테고리가 일반적으로 여러개이다!
+    ex) 카테고리 아래에 여성복, 남성복, 아동복
+    보는 관점을 기준으로 Many와 One이 정해진다!!
+    parent의 관점에서 child는 Many고, parent는 One이니까 @ManyToOne이 맞음!
+     */
     @ManyToOne
     @JoinColumn(name = "parent_id")
     private Category parent;
@@ -25,5 +31,45 @@ public class Category extends BaseEntity {
             joinColumns = @JoinColumn(name = "category_id"),
             inverseJoinColumns = @JoinColumn(name = "item_id"))
     private List<Item> items = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getParent() {
+        return parent;
+    }
+
+    public void setParent(Category parent) {
+        this.parent = parent;
+    }
+
+    public List<Category> getChild() {
+        return child;
+    }
+
+    public void setChild(List<Category> child) {
+        this.child = child;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
 
 }
